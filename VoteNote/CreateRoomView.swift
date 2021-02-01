c331c5c6489467f62b8f41caff8086c7c57804dd
PJKnightR <p.j.reagan99@gmail.com>
2021-01-31 21:47:50 -0500

    Added most room creation settings functionality for Sprint 1

--- VoteNote/CreateRoomView.swift
+++ VoteNote/CreateRoomView.swift
@@ -10,18 +10,66 @@ import SwiftUI
 
 
 struct CreateRoomView: View {
-  
+    //TODO: Send Room info to Database
+    //TODO: Create second version of this view to take in info from an existing room
+    @State var showAlert: Bool = false
+    
+    @State var userCapacity: Int = 20 {
+        didSet {
+            if userCapacity < 1 {
+                userCapacity = 1
+            }
+        }
+    }
+  @State var songsPerUser: Int = 4 {
+    didSet {
+        if songsPerUser < 1 {
+            songsPerUser = 1
+        }
+    }
+  }
+    
+  @State var roomName: String = ""
+  @State var roomDescription: String = ""
+    
   var body: some View {
     return NavigationView {
       ZStack {
         Color.white
         VStack {
-          Text("Create Room")
-          NavigationLink(
-            destination: Host_QueuePageView(),
-            label: {
-              Text("go to Host Queue Page")
-            })
+            
+            Form {
+                Section(header: Text("General Room Information")) {
+                    TextField("Room Name", text: $roomName)
+                    TextField("Room Description", text: $roomDescription)
+                }
+                
+                Section(header: Text("Settings")) {
+                    HStack {
+                        Text("User Capacity")
+                            .padding(.trailing)
+                        Stepper("\(userCapacity)", onIncrement: { userCapacity+=1}, onDecrement: { userCapacity-=1})
+                            .padding(.leading)
+                    }
+                    HStack {
+                        Text("Songs Per User")
+                            .padding(.trailing)
+                        Stepper("\(songsPerUser)", onIncrement: { songsPerUser+=1}, onDecrement: { songsPerUser-=1})
+                            .padding(.leading)
+                    }
+                }
+            }
+            
+            if self.roomName == "" || self.roomDescription == "" {
+                NavigationLink(
+                  destination: Host_QueuePageView(),
+                  label: {
+                    Text("Create Room")
+                  })
+                  .padding(.vertical)
+            }
+          
+          Spacer()
         }
       }
       .navigationBarHidden(true)
@@ -29,8 +77,19 @@ struct CreateRoomView: View {
   }
 }
 
+struct CreateRoomView_PreviewContainer: View {
+  
+    @State var userCapacity: Int = 20
+    @State var songsPerUser: Int = 4
+    
+    var body: some View {
+        CreateRoomView(userCapacity: userCapacity, songsPerUser: songsPerUser)
+    }
+}
+
 struct CreateRoomView_Previews: PreviewProvider {
   static var previews: some View {
-    CreateRoomView()
+    
+    CreateRoomView_PreviewContainer()
   }
 }
