deeebd0ab676a4edc87ae57fc7feee2fbee2e894
acramernc <acramernc@gmail.com>
2021-02-01 10:41:57 -0500

    firebase mk 7

--- VoteNote/AppDelegate.swift
+++ VoteNote/AppDelegate.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 //firebase import, pretty sure this goes here
-//import Firebase
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate, SPTSessionManagerDelegate, SPTAppRemoteDelegate, SPTAppRemotePlayerStateDelegate {
@@ -39,6 +39,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, SPTSessionManagerDelegate
     }()
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
