0f03a46606724c6cb4f4e4037d31dce1de89da0a
acramernc <acramernc@gmail.com>
2021-02-12 09:24:07 -0500

    added a db object in contentView

--- VoteNote/ContentView.swift
+++ VoteNote/ContentView.swift
@@ -8,17 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-  @ObservedObject var spotify = Spotify()
-  var body: some View {
-    VStack {
-    if (!spotify.loggedIn) {
-        LoginWithSpotifyView(spotify: spotify)
-          .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-      } else {
-        LandingPageView(spotify: spotify)
-      }
+    @ObservedObject var spotify = Spotify()
+    var body: some View {
+        VStack {
+            if (!spotify.loggedIn) {
+                LoginWithSpotifyView(spotify: spotify)
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            } else {
+                LandingPageView(spotify: spotify)
+            }
+        }
     }
-  }
+    
+    var db = dbConnection()
 }
 
 struct ContentView_Previews: PreviewProvider {
