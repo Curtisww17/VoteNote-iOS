5d02a971c64bcb3afc8bc58eef8e545d7d6deb93
Wesley Curtis <wcvikingdude@gmail.com>
2021-03-14 14:25:51 -0400

    test thing

---
+++ VoteNote/PreviouslyJoinedRoomsView.swift
@@ -0,0 +1,21 @@
+//
+//  PreviouslyJoinedRoomsView.swift
+//  VoteNote
+//
+//  Created by Wesley Curtis on 3/14/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct PreviouslyJoinedRoomsView: View {
+  @State var rooms: [String]
+  var body: some View {
+    return HStack {
+      Text("Destination")
+    }
+    .onAppear(){
+      
+    }
+  }
+}
